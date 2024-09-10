d564c5e6568d7d98e17d073fed49b47dcb4bd7a2
lala-p <sangmicell@gmail.com>
2024-09-10 15:49:15 +0900

    react-router-dom 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import App from './App.tsx'
+import { createBrowserRouter, RouterProvider, RouteObject, Outlet } from 'react-router-dom'
+// import './index.css'
+
+const routerStructure: RouteObject[] = [
+  {
+    path: "/",
+    element: <><h1>root</h1><Outlet /></>,
+    errorElement: <h1>error errorElement</h1>,
+    children: [
+      {
+        index: true,
+        element: <h1>index home</h1>,
+      },
+      {
+        path: "/generateTest",
+        element: <h1>generateTest</h1>,
+      },
+      {
+        path: "/vocabularyList",
+        element: <h1>vocabularyList</h1>,
+      },
+    ],
+  },
+]
+
+
+const router = createBrowserRouter(routerStructure)
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 )
