fe0e66e8fe83db73f37197027f466408a8f15511
lulu <sangmicell@gmail.com>
2024-09-12 00:06:11 +0900

    tailwind css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 import { createBrowserRouter, RouterProvider, RouteObject, Outlet } from 'react-router-dom'
-// import './index.css'
+import './index.css'
 
 const routerStructure: RouteObject[] = [
   {
     path: "/",
-    element: <><h1>root</h1><Outlet /></>,
+    element: <><h1>root</h1><div className="text-3xl bg-blue-500">Tailwind Css 적용 테스트</div><Outlet /></>,
     errorElement: <h1>error errorElement</h1>,
     children: [
       {
